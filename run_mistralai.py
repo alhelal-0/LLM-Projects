de63e201d228890b5b506d1f4768ad4333281268
Md Helaluddin <mdhelaluddin@Mds-Mac-mini.local>
2026-05-30 17:24:24 -0500

    Initial commit

---
+++ run_mistralai.py
@@ -0,0 +1,26 @@
+
+import os
+from mistralai import Mistral
+from mistralai.models import UserMessage
+
+os.environ["MISTRAL_API_KEY"] = "JjdDxg4A2ZJU7Jtkuj2zujQudUOB2c6K"
+
+client = Mistral(api_key=os.environ["MISTRAL_API_KEY"])
+
+messages = [
+    UserMessage(role="user", content="Create a good looking profile card in css and html")
+]
+
+response = client.chat.stream(
+    model="mistral-large-2407",
+    messages=messages,
+    stream=True,
+)
+
+content = []
+for event in response:
+    delta = event.data.choices[0].delta
+    if delta.content is not None:
+        content.append(delta.content)
+
+print("".join(content))
